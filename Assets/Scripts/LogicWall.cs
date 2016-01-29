64252e664bca424bc76691eea87991874d58da2f
jordibox <jordinformatica@gmail.com>
2016-01-29 23:36:57 +0100

    Tags añadidos

---
+++ Assets/Scripts/LogicWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicWall : MonoBehaviour {
+
+	public GameObject key;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision colision){
+		if (colision.gameObject.tag == "Player") {
+			float speed = colision.relativeVelocity.magnitude;
+			key.GetComponent<LogicKey>.DownKey (speed);
+		}
+	}
+}
