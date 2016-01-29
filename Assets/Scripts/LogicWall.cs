2c9ceb7c0334d8adfcb6a692bf5523615a218909
jordibox <jordinformatica@gmail.com>
2016-01-29 21:05:42 +0100

    Primer push

---
+++ Assets/Scripts/LogicWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicKey : MonoBehaviour {
+
+	public GameObject key;
+	public GameObject player;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			float speed=collision.relativeVelocity.magnitude;
+			key.GetComponent<LogicKey>.downKey (speed);
+		}
+	}
+}
