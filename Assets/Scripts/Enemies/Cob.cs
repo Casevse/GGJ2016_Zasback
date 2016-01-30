f9f2484f8a9875c090061ac5f519175fd3c03b4e
afp36 <afp36@alu.ua.es>
2016-01-30 09:48:08 +0100

    Enemig Cob
    Enemigo Cob terminado con los disparos.

--- Assets/Scripts/Enemies/Cob.cs
+++ Assets/Scripts/Enemies/Cob.cs
@@ -6,15 +6,17 @@ public class Cob : Enemy {
 	private Rigidbody2D rigidBody;
 	private float timeShoot;
 	private float delayShoot;
-	public Bullet bullet;
+	public GameObject bullet;
+	public int bulletSpeed;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D>();
 		initEnemy (1, 1.5f, 1, 1f);
 		rigidBody.gravityScale += fallSpeed;
-		timeShoot = Time.time - 1.0f;
-		delayShoot = 1.0f;
+		timeShoot = Time.time;
+		delayShoot = 2.0f;
+		bulletSpeed = 4;
 	}
 	
 	// Update is called once per frame
@@ -29,6 +31,24 @@ public class Cob : Enemy {
 			else{
 				rigidBody.velocity = new Vector2(-movSpeed, 0.0f);
 			}
+
+			if((Time.time - timeShoot) > delayShoot){
+				shoot(side);
+				timeShoot = Time.time;
+			}
+		}
+	}
+
+	void shoot(bool s){
+		GameObject clone;
+		if (s) {
+			Vector3 aux = new Vector3(transform.position.x + 0.5f, transform.position.y, 0.0f);
+			clone = (GameObject) Instantiate (bullet, aux, transform.rotation);
+			clone.GetComponent<Rigidbody2D>().velocity = new Vector2(4.0f, 0.0f);
+		} else {
+			Vector3 aux = new Vector3(transform.position.x - 0.5f, transform.position.y, 0.0f);
+			clone = (GameObject) Instantiate (bullet, aux, transform.rotation);
+			clone.GetComponent<Rigidbody2D>().velocity = new Vector2(-4.0f, 0.0f);
 		}
 	}
 }
