1d8d4eae11c4b90457e601ebffd02ba3280c84c9
afp36 <afp36@alu.ua.es>
2016-01-30 04:34:11 +0100

    Menu cambio
    Botones con sus posiciones y su interacción puesta.

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void buttonPlay(){
+		Application.LoadLevel("Game");
+	}
+
+	public void buttonHistory(){
+		Application.LoadLevel("History");
+	}
+
+	public void buttonControls(){
+		Application.LoadLevel("Controls");
+	}
+
+	public void buttonOptions(){
+		Application.LoadLevel("Options");
+	}
+
+	public void buttonExit(){
+		Application.Quit();
+	}
+}
